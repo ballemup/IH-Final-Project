2e13259986b27132947fdcf109248f3cc15a02dd
Joe <joeyballesteros.wfg@gmail.com>
2021-05-11 07:16:51 -0700

    joey 1st commit

---
+++ frontend/src/Components/Cocktails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Cocktails(props) {
+    return (
+        <div>
+            <div className = 'Drink1'>Drink 1</div>
+            <div className = 'Drink2'>Drink 2</div>
+            <div className = 'Drink3'>Drink 3</div>
+            <div className = 'Drink4'>Drink 4</div>
+            <div className = 'Drink5'>Drink 5</div>
+            <div className = 'Drink6'>Drink 6</div>
+        </div>
+    );
+}
+
+export default Cocktails;
