c6f91e0f3711c3cdd2a228b4218e1f6bec9895cb
Joe <joeyballesteros.wfg@gmail.com>
2021-05-14 13:25:55 -0700

    changes in cyo

--- frontend/src/Components/CreateYourOwn.js
+++ frontend/src/Components/CreateYourOwn.js
@@ -1,75 +1,77 @@
-import React from 'react';
-import './cocktails.css'
+import React, {useState} from 'react';
+import './cyo.css'
 // import {custom} from '../Custom/ingredients'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 
 
 function CreateYourOwn(props) {
     const Alcohol = ['vodka', 'whiskey', 'tequila', 'rum', 'champagne']
     const Juices= ['orange' , 'cranberry' , 'tomato' , 'lemon' ,  'lime' ]
-    const  Ingredients= ['Ginger', 'Mint', 'cinnamon']
-    const  Syrup= ['Blue Curacao','Pure Sugar Cane','Grenadine', 'Passion Fruit', 'Strawberry ','Sweetner Sugar Free','Vanilla','Mojito Mint']
-    const  Sodas= ['Soda water', 'Club Soda', 'Tonic Water', 'Ginger Ale', 'Ginger Beer','Lemon-Lime Soda', 'Cola', 'Root Beer']
+    const Ingredients= ['Ginger', 'Mint', 'cinnamon']
+    const Syrup= ['Blue Curacao','Pure Sugar Cane','Grenadine', 'Passion Fruit', 'Strawberry ','Sweetner Sugar Free','Vanilla','Mojito Mint']
+    const Sodas= ['Soda water', 'Club Soda', 'Tonic Water', 'Ginger Ale', 'Ginger Beer','Lemon-Lime Soda', 'Cola', 'Root Beer']
+
+    const [color,setColor]=useState('red');
+    const [textColor,setTextColor]=useState('white');
 
 
     const showAlcohol = () => {
         return Alcohol.map(eachIngredient => {
-        return <button style= {{width: "15vw", padding: "20px"}}type="button" className='btn btn-primary'> {eachIngredient}</button>
+        return <button> {eachIngredient}</button>
             })
 
     }
     const showJuices = () => {
         return Juices.map(eachIngredient => {
-        return <button style= {{width: "15vw", padding: "20px"}}type="button" className='btn btn-primary'> {eachIngredient}</button>
+        return <button> {eachIngredient}</button>
             })
 
     }
     const showIngredients = () => {
         return Ingredients.map(eachIngredient => {
-        return <button style= {{width: "15vw", padding: "20px"}}type="button" className='btn btn-primary'> {eachIngredient}</button>
+        return <button > {eachIngredient}</button>
             })
 
     }
     const showSyrup = () => {
         return Syrup.map(eachIngredient => {
-        return <button style= {{width: "15vw", padding: "20px"}}type="button" className='btn btn-primary'> {eachIngredient}</button>
+        return <button> {eachIngredient}</button>
             })
 
     }
     const showSodas = () => {
         return Sodas.map(eachIngredient => {
-        return <button style= {{width: "15vw", padding: "20px"}}type="button" className='btn btn-primary'> {eachIngredient}</button>
+        return <button>{eachIngredient}</button>
             })
 
     }
-    
-
-    // const listAll = custom.forEach(e => console.log(e))
 
     return (
-        <div>
+        <div className = "main">
             <h1>Create Your Own</h1>
             <h2>Choose Alcohol:</h2>
-            <div className="btn-group">
+            <div className="btn-group alcohol">
                 {showAlcohol()}
             </div>
             <h2>Choose Juices:</h2>
-            <div className="btn-group" >
+            <div className="btn-group juices" >
                 {showJuices()}
             </div>
             <h2>Choose Ingredients:</h2>
-            <div className="btn-group" >       
+            <div className="btn-group ingredients" >       
                 {showIngredients()}
             </div>
             <h2>Choose Syrup:</h2>
-            <div className="btn-group" >       
+            <div className="btn-group syrup" >       
                 {showSyrup()}
             </div>
             <h2>Choose Sodas:</h2>
-            <div className="btn-group" >       
+            <div className="btn-group sodas" >       
                 {showSodas()}
             </div>  
-            
         </div>
+        
     );
 }
 
